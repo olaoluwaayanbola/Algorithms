c50c8593fba3d6ffa1540a3146fad8fb25fca39b
olaoluwa <69906641+olaoluwaayanbola@users.noreply.github.com>
2023-03-20 06:14:12 +0100

    Create index2.js

---
+++ src/index2.js
@@ -0,0 +1,10 @@
+var canPlaceFlowers = function (flowerbed, n) {
+    for (let i = 0; i < flowerbed.length & n > 0; i++) {
+        if (flowerbed[i] == 0 && flowerbed[i + 1] !== 1 &&
+            flowerbed[i - 1] !== 1) {
+            flowerbed[i] = 1
+            n--
+        }
+    }
+    return n == 0
+};
