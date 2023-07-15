bf713266d31d3fc89c83ee229bed3060aafd5acd
olaoluwa <69906641+olaoluwaayanbola@users.noreply.github.com>
2023-07-15 11:01:37 -0700

    Update isPalindrome.js

--- leetcode/isPalindrome.js
+++ leetcode/isPalindrome.js
@@ -20,3 +20,21 @@ var isPalindrome = function (s) {
     }
     return oddcount <=  1
 };
+
+var isPalindrome = function (s) {
+    let arr = []
+    let rearr = []
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i].toLowerCase()
+        if (/[a-z0-9]/.test(char) && char !== " ") {
+            arr.push(char)
+        }
+    }
+    for (let i = s.length-1; i >= 0; i--) {
+        let char = s[i].toLowerCase()
+        if (/[a-z0-9]/.test(char) && char !== " ") {
+            rearr.push(char)
+        }
+    }
+    return arr.join("") === rearr.join("")
+};
