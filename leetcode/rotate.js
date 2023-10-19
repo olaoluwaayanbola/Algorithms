bc0512917dfbb3e4881a812459e6e275fde4a048
olaoluwa <69906641+olaoluwaayanbola@users.noreply.github.com>
2023-10-18 18:05:15 -0700

    Create rotate.js

---
+++ leetcode/rotate.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+  let size = nums.length 
+  k = k % size
+
+  const rotator_func = (start,end) => {
+    while(start <= end){
+      let hold_ele = nums[start]
+      nums[start] = nums[end]
+      nums[end] = hold_ele
+      start++
+      end--
+    }
+  }
+  if(k > 0){
+    rotator_func(0,size -1)
+    rotator_func(0,k-1)
+    rotator_func(k,size -1)
+  }
+};
